e55c916efa7dd315e44da412209f204555a1da13
academy_intern <academy_intern@academy-interns-MacBook-Pro.local>
2020-01-17 13:29:44 +0200

    first commit

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { InfoComponent } from './info/info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    InfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
